ba54eae9fd2f0a4c71db8d9641d5901f1d5e18dd
romulo nishiyama <romulonishiyama@gmail.com>
2023-05-22 17:00:28 -0300

    2

--- src/main/java/br/com/portugal/loja/modelo/Produto.java
+++ src/main/java/br/com/portugal/loja/modelo/Produto.java
@@ -3,6 +3,8 @@ package br.com.portugal.loja.modelo;
 import java.math.BigDecimal;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -11,6 +13,7 @@ import javax.persistence.Table;
 public class Produto {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	private String nome;
@@ -18,5 +21,37 @@ public class Produto {
 	private String descricao;
 
 	private BigDecimal preco;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
 
 }
